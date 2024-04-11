afaeddc8348f8540765b742a31e362f9c3596c98
Dennchiik <119806784+Dennchiik@users.noreply.github.com>
2024-04-11 18:59:09 +0300

    Add files via upload

---
+++ 02,04/02,04/02,04.cpp
@@ -0,0 +1,55 @@
+﻿//перше завдання
+/*#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+bool compareLength(const string& s1, const string& s2) {
+    return s1.length() < s2.length();
+}
+
+int main() {
+    vector<string> strings = {"apple", "banana", "cherry", "date", "fig"};
+
+    sort(strings.begin(), strings.end(), compareLength);
+
+    cout << "Відсортований вектор строк:" << endl;
+    for (const string& s : strings) {
+        cout << s << endl;
+    }
+
+    return 0;
+}
+*/
+//друге завдання
+/*#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+class CompareLength {
+public:
+    bool operator()(const string& s1, const string& s2) {
+        return s1.length() < s2.length();
+    }
+};
+
+int main() {
+    vector<string> strings = {"apple", "banana", "cherry", "date", "fig"};
+
+    CompareLength compare;
+
+    sort(strings.begin(), strings.end(), compare);
+
+    cout << "Відсортований вектор строк:" << endl;
+    for (const string& s : strings) {
+        cout << s << endl;
+    }
+
+    return 0;
+}
+*/
